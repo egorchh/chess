2befc46471d95af80292fca5c7e702faddc766df
Egor Podolskij <podolskijegor51@gmail.com>
2022-10-14 01:41:25 +0300

    Added: logic of figures' movement

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -27,8 +27,16 @@ export class Cell {
     this.id = Math.random();
   }
 
-  isEmpty() {
+  isEmpty(): boolean {
     return this.figure === null
+  }
+
+  isEnemy(target: Cell): boolean {
+    if (target.figure) {
+      return this.figure?.color !== target.figure?.color;
+    }
+
+    return false;
   }
 
   isEmptyVertical(target: Cell): boolean {
@@ -49,17 +57,49 @@ export class Cell {
   }
 
   isEmptyHorizontal(target: Cell): boolean {
-    return true;  
+    if (this.y !== target.y) {
+      return false;
+    }
+
+    const min = Math.min(this.x, target.x)
+    const max = Math.max(this.x, target.x)
+
+    for (let x = min + 1; x < max; x++) {
+      if (!this.board.getCell(x, this.y).isEmpty()) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   isEmptyDiagonal(target: Cell): boolean {
+    const absX = Math.abs(target.x - this.x);
+    const absY = Math.abs(target.y - this.y);
+    if (absX !== absY)
+      return false;
+
+    const dy = this.y < target.y ? 1 : -1;
+    const dx = this.x < target.x ? 1 : -1;
+
+    for (let i = 1; i < absY; i++) {
+      if (!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) {
+        return false;
+      }
+    }
+
     return true;
+  }
+
+  setFigure(figure: Figure) {
+    this.figure = figure;
+    this.figure.cell = this;
   }
 
   moveFigure(target: Cell) {
     if (this.figure && this.figure?.canMove(target)) {
       this.figure.moveFigure(target);
-      target.figure = this.figure;
+      target.setFigure(this.figure);
       this.figure = null;
     }
   }
